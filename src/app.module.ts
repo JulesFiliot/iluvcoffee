f19874384f5219655a0fd104a6efc684629b0a9a
JulesFiliot <jules.filiot@vo2-group.com>
2022-01-11 22:54:48 -0500

    [FIX] error on event and database delta

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
 imports: [
@@ -17,6 +18,7 @@ imports: [
 		autoLoadEntities: true,
 		synchronize: true,
 	}),
+	CoffeeRatingModule,
 ],
 controllers: [AppController],
 providers: [AppService],
