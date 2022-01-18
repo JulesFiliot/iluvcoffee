f1e25ceddbe986b62b57c2a60117a21b2a3c2d3c
JulesFiliot <jules.filiot@cpe.fr>
2022-01-17 23:21:23 -0500

    [ADD] first unit testing w/ jest

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Flavor } from './flavor.entity';
+
 @Entity()
 export class Coffee {
 	@PrimaryGeneratedColumn()
