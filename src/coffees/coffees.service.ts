52b152f465aef89327151af0bef60e06a3e91584
JulesFiliot <jules.filiot@cpe.fr>
2022-01-20 23:07:56 -0500

    [WIP] coffee service find one method unit testing

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Connection, Repository } from 'typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,8 +7,6 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Event } from '../events/entities/event.entity';
-import coffeesConfig from './config/coffees.config';
-import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class CoffeesService {
@@ -18,11 +16,7 @@ export class CoffeesService {
 		@InjectRepository(Flavor)
 		private readonly flavorRepository: Repository<Flavor>,
 		private readonly connection: Connection,
-		@Inject(coffeesConfig.KEY)
-		private readonly coffeesConfiguration: ConfigType<typeof coffeesConfig>,
-	) {
-		console.log(coffeesConfiguration.foo);
-	}
+	) {}
 
 	findAll(paginationQuery: PaginationQueryDto) {
 		const { limit, offset } = paginationQuery;
